3190524c5af29b731d248b8ac19aa7f6c53ae5c3
skye-pa1n <sonicgames772@gmail.com>
2024-10-17 11:13:35 -0400

    smol fix

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -605,18 +605,18 @@ static int qcom_cpufreq_hw_read_lut(struct platform_device *pdev,
 			cpu_dev = get_cpu_device(cpu);
 			if (!cpu_dev)
 				continue;
-			dev_pm_opp_add(cpu_dev, c->table[i].frequency * 1000, volt);
-	                if (cpu == 0) {
-				c->table[i].frequency = 300000;
-				c->table[i].frequency = 691200;
-				c->table[i].frequency = 883200;
-				c->table[i].frequency = 1171200;
-				c->table[i].frequency = 1344000;
-				c->table[i].frequency = 1516800;
-				c->table[i].frequency = 1708800;
-                                c->table[i].frequency = 1920400;
-				c->table[i].frequency = 2246400;	
-			}       
+			dev_pm_opp_add(cpu_dev, c->table[i].frequency * 1000, volt);	
+			if (cpu == 0) {
+			        c->table[i++].frequency = 300000;
+			        c->table[i++].frequency = 691200;
+			        c->table[i++].frequency = 1171200;
+				c->table[i++].frequency = 1344000;
+				c->table[i++].frequency = 1516800;
+				c->table[i++].frequency = 1708800;
+				c->table[i++].frequency = 1804800;
+                                c->table[i++].frequency = 2054400;
+				c->table[i++].frequency = 2246400;
+			}
 		}
 	}
 
