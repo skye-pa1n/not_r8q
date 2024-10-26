d50816f366965e39b3047398ef35f027ed640140
skye-pa1n <sonicgames772@gmail.com>
2024-10-26 13:42:10 -0400

    uhh revert everything bcuz i messed up

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Oct 20 13:29:29 EDT 2024"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "debian"
+#define LINUX_COMPILER "Neutron clang version 17.0.0 (https://github.com/llvm/llvm-project.git 7f48154ca11fee6579fe29a51ab2a329424d31c4), LLD 17.0.0"
---
+++ skye.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+LLVM_PATH="/home/skye/bomb/clang/bin/"
+TC_PATH="/home/skye/bomb/clang/bin/"
+GCC_PATH="/usr/bin/"
+KERNEL_NAME=not-perf+
+MAKE="./makeparallel"
+BUILD_ENV="CC=${TC_PATH}clang CROSS_COMPILE=${GCC_PATH}aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 PATH=$LLVM_PATH:$LLD_PATH:$PATH"  
+
+KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+rm -rf AnyKernel3/dtb
+make clean
+rm -rf .version
+rm -rf .local
+make mrproper
+make O=/home/skye/bomb/out clean
+make O=/home/skye/bomb/out mrproper
+make O=/home/skye/bomb/out ARCH=arm64 $BUILD_ENV not_defconfig
+
+DATE_START=$(date +"%s")
+
+make -j$(nproc --all) O=/home/skye/bomb/out ARCH=arm64 $KERNEL_MAKE_ENV $BUILD_ENV Image
+
+make -j$(nproc --all) O=/home/skye/bomb/out ARCH=arm64 $KERNEL_MAKE_ENV $BUILD_ENV dtbs
+
+DTB_OUT="/home/skye/bomb/out/arch/arm64/boot/dts/vendor/qcom"
+IMAGE="/home/skye/bomb/out/arch/arm64/boot/Image"
+
+cat $DTB_OUT/*.dtb > AnyKernel3/dtb
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo "Tempo de compilação: $(($DIFF / 60)) minutos(s) and $(($DIFF % 60)) segundos."
+
+cp $IMAGE AnyKernel3/Image
+cd AnyKernel3
+rm *.zip
+zip -r9 ${KERNEL_NAME}.zip .
