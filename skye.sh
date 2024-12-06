5b87d808ed1fb5d56036139cf1e43b4cde6ad287
skye-pa1n <sonicgames772@gmail.com>
2024-12-06 16:17:29 -0500

    dtb: Proper A15 Support

--- skye.sh
+++ skye.sh
@@ -3,7 +3,7 @@
 LLVM_PATH="/home/skye/bomb/clang/bin/"
 TC_PATH="/home/skye/bomb/clang/bin/"
 GCC_PATH="/usr/bin/"
-KERNEL_NAME=not-perf+
+KERNEL_NAME=notK-perf+
 MAKE="./makeparallel"
 BUILD_ENV="CC=${TC_PATH}clang CROSS_COMPILE=${GCC_PATH}aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 PATH=$LLVM_PATH:$LLD_PATH:$PATH"  
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
@@ -19,7 +19,7 @@ make O=/home/skye/bomb/out ARCH=arm64 $BUILD_ENV not_defconfig
 make -j10 O=/home/skye/bomb/out ARCH=arm64 $KERNEL_MAKE_ENV $BUILD_ENV dtbs
 
 DTB_OUT="/home/skye/bomb/out/arch/arm64/boot/dts/vendor/qcom"
-cat $DTB_OUT/*.dtb > AnyKernel3/840dtb
+cat $DTB_OUT/*.dtb > AnyKernel3/dtb
 
 make -j$(nproc --all) O=/home/skye/bomb/out ARCH=arm64 $KERNEL_MAKE_ENV $BUILD_ENV Image
 IMAGE="/home/skye/bomb/out/arch/arm64/boot/Image"
