55c72c22d83455700a4abd54efeb8a192a4d426a
skye-pa1n <sonicgames772@gmail.com>
2025-01-20 13:24:16 -0500

    include: linux: mm.h: set VM_MAX_READAHEAD to 8192 & VM_MIN_READAHEAD to 3072kb

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2584,8 +2584,8 @@ int __must_check write_one_page(struct page *page);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_MAX_READAHEAD	128	/* kbytes */
-#define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
+#define VM_MAX_READAHEAD	8192	/* kbytes */
+#define VM_MIN_READAHEAD	3072	/* kbytes (includes current page) */
 extern int mmap_readaround_limit;
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
