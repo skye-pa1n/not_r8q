fb37d8218d5cfe920329a7c258c7fd8f64498fac
skye-pa1n <sonicgames772@gmail.com>
2025-01-23 13:16:03 -0500

    fs: add can_umount [5.10.9]

--- fs/namespace.c
+++ fs/namespace.c
@@ -1802,6 +1802,22 @@ static int can_umount(const struct path *path, int flags)
 		 return -EPERM;
 	 return 0;
  }
+ 
+static int can_umount(const struct path *path, int flags)
+{
+	struct mount *mnt = real_mount(path->mnt);
+	if (!may_mount())
+		return -EPERM;
+	if (path->dentry != path->mnt->mnt_root)
+		return -EINVAL;
+	if (!check_mnt(mnt))
+		return -EINVAL;
+	if (mnt->mnt.mnt_flags & MNT_LOCKED) /* Check optimistically */
+		return -EINVAL;
+	if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))
+		return -EPERM;
+	return 0;
+}
 
 int path_umount(struct path *path, int flags)
  {
