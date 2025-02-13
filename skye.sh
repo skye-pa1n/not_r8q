76ea3af581e3aec125d7f1b3dd8a0c6a6927402a
skye // pa1n <sonicgames772@gmail.com>
2025-02-13 14:51:48 -0500

    skye: micro update include compiler name on zip

--- skye.sh
+++ skye.sh
@@ -3,33 +3,34 @@ LLVM_PATH="/home/skye/bomb/clang/bin/"
 TC_PATH="/home/skye/bomb/clang/bin/"
 GCC_PATH="/usr/bin/"
 LLD_PATH="/usr/bin/"
-KERNEL_NAME=not-endless+
+CLANGV="21"
+KERNEL_NAME="not-endless+"
 MAKE="./makeparallel"
-BUILD_ENV="CC=${TC_PATH}clang CROSS_COMPILE=${TC_PATH}aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 PATH=$LLVM_PATH:$LLD_PATH:$PATH"  
+BUILD_ENV="CC=${TC_PATH}clang-${CLANGV} CROSS_COMPILE=${TC_PATH}aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 PATH=$LLVM_PATH:$LLD_PATH:$PATH"  
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
 rm -rf /home/skye/bomb/out/arch/arm64/boot/Image
 rm -rf /home/skye/bomb/AnyKernel3/dtb
+rm -rf /home/skye/bomb/dtbo.img
 rm -rf .version
 rm -rf .local
 make O=/home/skye/bomb/out clean
 make O=/home/skye/bomb/out ARCH=arm64 $BUILD_ENV not_defconfig
 
-make -j10 O=/home/skye/bomb/out ARCH=arm64 $KERNEL_MAKE_ENV $BUILD_ENV dtbs
+make -j32 O=/home/skye/bomb/out ARCH=arm64 $BUILD_ENV dtbs
 DTB_OUT="/home/skye/bomb/out/arch/arm64/boot/dts/vendor/qcom"
-cat $DTB_OUT/*.dtb > /home/skye/bomb/AnyKernel3/aosp.dtb
+cat $DTB_OUT/*.dtb > /home/skye/bomb/AnyKernel3/aosp14.dtb
 
-make -j10 O=/home/skye/bomb/out ARCH=arm64 $KERNEL_MAKE_ENV $BUILD_ENV dtbo.img
+make -j32 O=/home/skye/bomb/out ARCH=arm64 $KERNEL_MAKE_ENV $BUILD_ENV dtbo.img
 DTBO_OUT="/home/skye/bomb/out/arch/arm64/boot"
-rm -rf /home/skye/bomb/dtbo.img
 cp $DTBO_OUT/dtbo.img /home/skye/bomb/dtbo.img
 
-make -j$(nproc --all) O=/home/skye/bomb/out ARCH=arm64 $KERNEL_MAKE_ENV $BUILD_ENV Image
+make -j32 O=/home/skye/bomb/out ARCH=arm64 $KERNEL_MAKE_ENV $BUILD_ENV Image
 IMAGE="/home/skye/bomb/out/arch/arm64/boot/Image"
-cp $IMAGE /home/skye/bomb/AnyKernel3/ImageA
+cp $IMAGE /home/skye/bomb/AnyKernel3/AOSP
 
 cd /home/skye/bomb/AnyKernel3
 rm *.zip
-zip -r9 ${KERNEL_NAME}-$(date +"%Y%m%d")-r8q.zip .
+zip -r9 ${KERNEL_NAME}${CLANGV}.0.0git-$(date +"%Y%m%d")-r8q.zip .
 echo "The bomb has been planted."
 
