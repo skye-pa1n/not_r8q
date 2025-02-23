d027b37db72d9f23e0b0eb68ca52b7dc3f2223b4
skye-pa1n <sonicgames772@gmail.com>
2025-02-22 21:00:10 -0300

    test

--- skye.sh
+++ skye.sh
@@ -19,7 +19,7 @@ make O=/home/skye/bomb/out ARCH=arm64 $BUILD_ENV not_defconfig
 
 make -j12 O=/home/skye/bomb/out ARCH=arm64 $BUILD_ENV dtbs
 DTB_OUT="/home/skye/bomb/out/arch/arm64/boot/dts/vendor/qcom"
-cat $DTB_OUT/*.dtb > /home/skye/bomb/AnyKernel3/aosp14.dtb
+cat $DTB_OUT/*.dtb > /home/skye/bomb/AnyKernel3/aosp.dtb
 
 make -j12 O=/home/skye/bomb/out ARCH=arm64 $KERNEL_MAKE_ENV $BUILD_ENV dtbo.img
 DTBO_OUT="/home/skye/bomb/out/arch/arm64/boot"
