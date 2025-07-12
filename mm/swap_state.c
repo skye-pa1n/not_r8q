2ed4ed418ca025789f9b9c2096567d6fa53c794c
Yu Zhao <yuzhao@google.com>
2025-07-12 17:07:01 +0800

    mm: mglru fix on qcom baseline
    Rebase the 4.19 mglru patches on qcom baseline
    Add vm events for more information
    Moto-CRs-Fixed:(CR)
    
    Change-Id: I81bfef4c09340ff0564cb37de915a2ce3fd0526d
    Signed-off-by: a17671 <a17671@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2281592
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/2481015
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>
    Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

--- mm/swap_state.c
+++ mm/swap_state.c
@@ -359,7 +359,6 @@ struct page *__read_swap_cache_async(swp_entry_t entry, gfp_t gfp_mask,
 	struct page *found_page, *new_page = NULL;
 	struct address_space *swapper_space = swap_address_space(entry);
 	int err;
-	void *shadow;
 
 	*new_page_allocated = false;
 
